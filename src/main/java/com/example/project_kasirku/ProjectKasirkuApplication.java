2cd1c38df97b528c437ab7abf11eac2c438be96a
alwafauzan123 <alwafauzan123@gmail.com>
2024-09-05 18:27:17 +0700

    init project

---
+++ src/main/java/com/example/project_kasirku/ProjectKasirkuApplication.java
@@ -0,0 +1,13 @@
+package com.example.project_kasirku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectKasirkuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectKasirkuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/project_kasirku/ProjectKasirkuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.project_kasirku;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectKasirkuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
